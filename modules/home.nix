0304ca209ff7ecb1bd093b993dd0a558b322892f
brhynders <brandonmrhynders@gmail.com>
2025-12-28 09:28:27 -0500

    fix home.nix

--- modules/home.nix
+++ modules/home.nix
@@ -1,13 +1,12 @@
 { config, pkgs, lib, home-manager, ... }:
 
 {
+  imports = [ home-manager.nixosModules.home-manager ];
+
   home-manager.users.brandon = {
     home.stateVersion = "23.05";
     home.file = {
-      ".bashrc" = {
-        source = ./config/.bashrc;
-        overwrite = true;
-      };
+      ".bashrc".source = ./config/.bashrc;
     };
   };
 }
