0169cf51e877d56f4e0aed9da04e127d439df83e
brhynders <brandonmrhynders@gmail.com>
2025-12-28 12:42:33 -0500

    cosmic desktop

--- modules/desktop-environment.nix
+++ modules/desktop-environment.nix
@@ -7,6 +7,11 @@
     };
 
     services.xserver.enable = true;
-    services.desktopManager.gnome.enable = true;
-    services.displayManager.gdm.enable = true;
+    #services.desktopManager.gnome.enable = true;
+    #services.displayManager.gdm.enable = true;
+      # Enable the COSMIC login manager
+  services.displayManager.cosmic-greeter.enable = true;
+
+  # Enable the COSMIC desktop environment
+  services.desktopManager.cosmic.enable = true;
 }
