15ad86e22f502edec6306fd7e6c648700fa12955
brhynders <brandonmrhynders@gmail.com>
2025-12-28 09:19:11 -0500

    fix home.nix

--- modules/home.nix
+++ modules/home.nix
@@ -1,38 +1,42 @@
 { config, lib, pkgs, home-manager, ... }:
 
 let
-  dotfilesHome = config.home.homeDirectory + "/dotfiles/home";
-  dotfilesHomeSlash = dotfilesHome + "/";  # precompute string with trailing slash
-
-  # Recursive function to collect all files
-  collectFiles = dir:
-    let
-      entries = builtins.attrNames (builtins.readDir dir);
-    in
-      lib.foldl' (acc: name:
-        let
-          path = dir + "/" + name;
-        in
-          if builtins.isDirectory path then
-            acc // collectFiles path
-          else
-            let
-              relPath = lib.replaceStrings [dotfilesHomeSlash] [""] path;
-            in
-              acc // {
-                "${relPath}" = {
-                  source = path;
-                  overwrite = true;
-                };
-              }
-      ) {} entries;
+  # optional: define username for convenience
+  username = "brandon";
 in
 
 {
   imports = [ home-manager.nixosModules.home-manager ];
 
-  home-manager.users.brandon = {
-    home.stateVersion = "23.05"; # required
+  home-manager.users.${username} = let
+    dotfilesHome = "/home/${username}/dotfiles/home";   # must hardcode or use config.users.username.home
+    dotfilesHomeSlash = dotfilesHome + "/";
+    
+    # Recursive function to collect all files
+    collectFiles = dir:
+      let
+        entries = builtins.attrNames (builtins.readDir dir);
+      in
+        lib.foldl' (acc: name:
+          let
+            path = dir + "/" + name;
+          in
+            if builtins.isDirectory path then
+              acc // collectFiles path
+            else
+              let
+                relPath = lib.replaceStrings [dotfilesHomeSlash] [""] path;
+              in
+                acc // {
+                  "${relPath}" = {
+                    source = path;
+                    overwrite = true;
+                  };
+                }
+        ) {} entries;
+  in
+  {
+    home.stateVersion = "23.05";  # required
     home.file = collectFiles dotfilesHome;
   };
 }
