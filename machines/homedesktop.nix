14514905a477366431cacff78fe59c05b2bc97eb
brhynders <brandonmrhynders@gmail.com>
2025-12-27 21:19:36 -0500

    fix indentation in niri config

--- machines/homedesktop.nix
+++ machines/homedesktop.nix
@@ -6,7 +6,7 @@
   imports =
     [
       ../modules/boilerplate.nix
-      ../modules/niri-desktop-environment.nix
+      ../modules/desktop-environment.nix
       ../modules/users.nix
       ../modules/packages.nix
       (modulesPath + "/installer/scan/not-detected.nix")
